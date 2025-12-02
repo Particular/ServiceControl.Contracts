cd1605205993ac39c02598f420a1225ba5b1598b
Szymon Pobiega <szymon.pobiega@gmail.com>
2025-12-02 21:12:58 +0100

    Added an event for edit-and-retry feature (#438)

---
+++ src/ServiceControl.Contracts/MessageEditedAndRetried.cs
@@ -0,0 +1,13 @@
+﻿namespace ServiceControl.Contracts
+{
+    /// <summary>
+    /// Event emitted by ServiceControl when edit-and-retry feature is used.
+    /// </summary>
+    public class MessageEditedAndRetried
+    {
+        /// <summary>
+        /// The unique identifier of the message that was edited
+        /// </summary>
+        public string FailedMessageId { get; set; }
+    }
+}
